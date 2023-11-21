d73336401c7f20eb181336d2bf8b5ca2e3004ddc
Johan Melin <soldierheart@hotmail.com>
2023-11-21 10:14:49 +0100

    lorem text

--- src/components/canvas/Rectangle.jsx
+++ src/components/canvas/Rectangle.jsx
@@ -5,7 +5,9 @@ const topBarHeight = CONSTANTS.TOPBAR_HEIGHT;
 const gridSize = CONSTANTS.GRID_SIZE;
 import PropTypes from 'prop-types';
 
-const Rectangle = ({ rectangles, style, color }) => {
+const loremIpsumText = `lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`.split(' ');
+
+const Rectangle = ({ rectangles, style, color, displayLorem }) => {
   return rectangles.map((r, i) => (
     <div
       key={i}
@@ -15,30 +17,32 @@ const Rectangle = ({ rectangles, style, color }) => {
         top: topBarHeight + r.y * gridSize, 
         width: r.w * gridSize || 20, 
         height: r.h * gridSize || 20, 
+        fontSize: r.h * gridSize * 0.8 || 20,
         backgroundColor: color
       }}
-    ></div>
+    >{displayLorem  && i < loremIpsumText.length && `${loremIpsumText.slice(i).join(' ')}`}</div>
   ));
 };
 
 export const Btn = ({rectangles}) => <Rectangle rectangles={rectangles} style={styles.btn} />
-export const Txt = ({rectangles}) => <Rectangle rectangles={rectangles} style={styles.txt}/>
+export const Txt = ({rectangles}) => <Rectangle rectangles={rectangles} style={styles.txt} displayLorem ></Rectangle>
 export const Other = ({rectangles}) => <Rectangle rectangles={rectangles} style={styles.other}/>
 export const Input = ({rectangles}) => <Rectangle rectangles={rectangles} style={styles.input}/>
 
 const RectPropTypes = {
-    rectangles: PropTypes.arrayOf(
-        PropTypes.shape({
-            x: PropTypes.number.isRequired,
-            y: PropTypes.number.isRequired,
-            w: PropTypes.number.isRequired,
-            h: PropTypes.number,
-        })
-    ).isRequired,
+  rectangles: PropTypes.arrayOf(
+    PropTypes.shape({
+      x: PropTypes.number.isRequired,
+      y: PropTypes.number.isRequired,
+      w: PropTypes.number.isRequired,
+      h: PropTypes.number,
+    })
+  ).isRequired,
 };
 
+
 Btn.propTypes = RectPropTypes;
-Txt.propTypes = RectPropTypes;
+Txt.propTypes = RectPropTypes,
 Other.propTypes = RectPropTypes;
 Input.propTypes = RectPropTypes;
 
