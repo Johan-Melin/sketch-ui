2c748841c73e310fedf9e3daa0f25f8080f75694
Johan Melin <soldierheart@hotmail.com>
2023-12-04 13:38:52 +0100

    prevent default topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,14 +2,23 @@ import styles from './TopBar.module.css'
 import PropTypes from 'prop-types';
 import HoverIcon from './topbar/HoverIcon'
 import { CONSTANTS } from './styles/constants.js';
+import { useRef } from 'react';
 
 export default function TopBar({onToolChange, /*onShowGridChange*/}) {
+  const topBar = useRef(null);
+  const handleTouchStart = (event) => {
+    event.preventDefault();
+  };
+  
+  // Prevent refresh page with slide
+  topBar.current.addEventListener('touchstart', handleTouchStart, { passive: false });
+
   const handleToolChange = (event) => {
     onToolChange(event.target.value);
   };
   
   return (
-    <div className={styles.topBar} style={{ height: `${CONSTANTS.TOPBAR_HEIGHT}px` }}>
+    <div className={styles.topBar}  ref={topBar} style={{ height: `${CONSTANTS.TOPBAR_HEIGHT}px` }}>
       <div>
         <HoverIcon value="text" handleToolChange={handleToolChange} />
         <HoverIcon value="other" handleToolChange={handleToolChange} />
